57e3d2446e81792f8bb4a6b23145f4079762f43a
saihgupr <121105543+saihgupr@users.noreply.github.com>
2026-03-08 13:50:45 -0400

    Install update

--- homeassistant-editor/run.sh
+++ homeassistant-editor/run.sh
@@ -4,7 +4,7 @@ export HOST="${HOST:-0.0.0.0}"
 export PORT="${PORT:-54002}"
 
 echo "======================================"
-echo "Home Assistant Editor v1.0.4"
+echo "Home Assistant Editor v1.1.0-beta.2"
 echo "======================================"
 echo "Starting server..."
 echo "======================================"
